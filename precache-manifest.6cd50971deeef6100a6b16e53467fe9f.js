6984eae80bf36786ac61f5d69bf2a2a185d2c684
Thomas Adams <tqsare@gmail.com>
2019-07-01 22:42:10 -0700

    Updates

---
+++ precache-manifest.6cd50971deeef6100a6b16e53467fe9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fa6abe2634534c08b6663ad010ebfbd",
+    "url": "/Dice/index.html"
+  },
+  {
+    "revision": "b24419cb02522458b403",
+    "url": "/Dice/static/css/main.e7460170.chunk.css"
+  },
+  {
+    "revision": "2790c44532533aa1f119",
+    "url": "/Dice/static/js/2.2668f2aa.chunk.js"
+  },
+  {
+    "revision": "b24419cb02522458b403",
+    "url": "/Dice/static/js/main.e56fc1c2.chunk.js"
+  },
+  {
+    "revision": "fe59f8e899c32258595d",
+    "url": "/Dice/static/js/runtime~main.620d72cf.js"
+  }
+]);
